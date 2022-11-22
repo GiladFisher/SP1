c89f3ad38446558bef0f62b809a9cc9a6b75f21b
GiladFisher <giladfisher98@gmail.com>
2022-11-22 12:11:10 +0200

    fixed print lines

--- main.c
+++ main.c
@@ -20,43 +20,43 @@ if ( y < x )
 
 int i;
 
-printf("The Armstrong numbers are: ");
+printf("The Armstrong numbers are:");
 for(i=x ; i<=y ; i++) 
 {   
     if( isArmstrong(i) )
     {
-        printf("%d ",i);
+        printf(" %d",i);
     }
 }
 printf("\n");
 
 
-printf("The Palindromes are: ");
+printf("The Palindromes are:");
 for(i=x ; i<=y ; i++)
 {
     if( isPalindrome(i) )
     {
-        printf("%d ",i);
+        printf(" %d",i);
     }
 }
 printf("\n");
 
-printf("The Prime numbers are: ");
+printf("The Prime numbers are:");
 for(i=x ; i<=y ; i++)
 {
     if( isPrime(i) )
     {
-        printf("%d ",i);
+        printf(" %d",i);
     }
 }
 printf("\n");
 
-printf("The Strong numbers are: ");
+printf("The Strong numbers are:");
 for(i=x ; i<=y ; i++)
 {
     if( isStrong(i) )
     {
-        printf("%d ",i);
+        printf(" %d",i);
     }
 }
 printf("\n");
